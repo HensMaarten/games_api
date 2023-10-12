ee840b6b2abb39b36f25c75a190b2b3de67c4ac8
HensMaarten <maarten.hens@telenet.be>
2023-10-12 13:37:32 +0200

    edit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const serverless = require('serverless-http');
@@ -8,6 +9,7 @@ const router = require('./routes');
 const app = express();
 //test
 app.use(express.json());
+app.use(cors());
 app.use('/.netlify/functions/api',router);
 
 mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true});
